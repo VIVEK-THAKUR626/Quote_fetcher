e0e5362412dbb172ad6352566b85421ebed83b73
VIVEK-THAKUR626 <vistaar4324@gmail.com>
2025-08-27 22:51:03 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const quotes = document.getElementById("content");
 const button = document.getElementById("fetch");
-const API_KEY = "api-key";
+const API_KEY = "OWkpx+GWCuLTqy9fzT2+lg==Ei75yBCFTg1wAjgy";
+let state = "pending";
 
 function displayQuote() {
     fetch("https://api.api-ninjas.com/v1/quotes",{
         headers: {'X-Api-Key': API_KEY}
     })
-    .then((response) => response.json())
+    .then((response) =>response.json())
     .then((data)=>{
         quotes.textContent = `"${data[0].quote}" - ${data[0].author}`;
     })
     .catch((error) => {
-      quote.textContent = "Failed to fetch quote.";
+    //   state = "fulfilled";
+      quotes.textContent = "Failed to fetch quote.";
       console.log(error);
     });
 }
